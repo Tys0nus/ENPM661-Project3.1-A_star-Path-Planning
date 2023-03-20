cdd6c3c6fb77dc14a8aae9757da7f77ddba7d250
Aditya Chaugule <adityachaugule1997@gmail.com>
2023-03-20 01:46:22 -0400

    -feature -main -rev00 mk01

---
+++ main.py
@@ -0,0 +1,26 @@
+import a_star_ankur_aditya as astr
+import visualize as vis
+
+if __name__ == "__main__":
+    print("-----------------------")
+    print("Path Planning: A star")
+    print("-----------------------")
+    x, y, th = [int(x) for x in input("Enter the Start Coordinates: ").split()] 
+    
+    if not (0 <= x < 600) or not (0 <= y < 250) or not (th in [0, 30, -30, 60, -60]):
+        raise ValueError("Start coordinates are not acceptable")
+    
+    y = 250 - y
+
+    start = (x,y, th)
+
+    x, y, th = [int(x) for x in input("Enter the Goal Coordinates: ").split()] 
+    if not (0 <= x < 600) or not (0 <= y < 250) or not (th in [0, 30, -30, 60, -60]):
+        raise ValueError("Goal coordinates are not acceptable")
+    
+    y = 250 - y
+    goal = (x,y, th)
+
+    canvas = vis.main(start,goal)
+    # astr.execute(start,goal)
+
